e8dc263e1a2c27273a597b294e049587fa7ea88e
haleywitherell <hwitherell22@gmail.com>
2021-09-30 13:56:05 -0400

    added employee tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -10,5 +10,36 @@ test('If we create a "new" Employee is should make a new object', () => {
 test('Adding a string to the first paramater creates a value for the property name of the object', () => {
     const testObj = new Employee('Haley')
 
+    expect(testObj.name).toBe('Haley')
+})
+
+test('Adding a string to the first paramater creates a value for the property id of the object', () => {
+    const testObj = new Employee('1234')
+
+    expect(testObj.id).toBe('1234')
+})
+
+test('Adding a string to the first paramater creates a value for the property email of the object', () => {
+    const testObj = new Employee('haley@gmail.com')
+
+    expect(testObj.email).toBe('haley@gmail.com')
+})
+
+test('Adding a string to the first paramater creates a value for the property name of the object', () => {
+    const testObj = new Employee('Haley')
+
     expect(testObj.getName()).toBe('Haley')
 })
+
+test('Adding a string to the first paramater creates a value for the property id of the object', () => {
+    const testObj = new Employee('Haley')
+
+    expect(testObj.getId()).toBe('1234')
+})
+
+test('Adding a string to the first paramater creates a value for the property id of the object', () => {
+    const testObj = new Employee('haley@gmail.com')
+
+    expect(testObj.getEmail()).toBe('haley@gmail.com')
+})
+
