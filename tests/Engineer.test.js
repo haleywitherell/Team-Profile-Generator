9ab765f3dddfd21a56f8962dab29b4f868c66690
haleywitherell <hwitherell22@gmail.com>
2021-10-03 17:07:35 -0400

    engineer tests

---
+++ tests/Engineer.test.js
@@ -0,0 +1,20 @@
+const Engineer = require("../lib/Engineer");
+
+
+test('Adding a string to the first paramater creates a value for the property github usermane of the object', () => {
+    const testObj = new Engineer('testgithub')
+
+    expect(testObj.github).toBe('testgithub')
+})
+
+test('Adding a string to the first paramater creates a value for the github usermane function of the object', () => {
+    const testObj = new Employee('testgithub')
+
+    expect(testObj.getGithub()).toBe('testgithub')
+})
+
+test('Adding a string to the first paramater creates a value for the role function of the object', () => {
+    const testObj = new Employee('Engineer')
+
+    expect(testObj.getRole()).toBe('Engineer')
+})
