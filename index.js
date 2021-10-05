09e1aa99fd1f078d1ee7d6adbe3a68241135e728
haleywitherell <hwitherell22@gmail.com>
2021-10-05 19:04:10 -0400

    updates

--- index.js
+++ index.js
@@ -160,13 +160,12 @@ function generateHTML(){
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Test</title>
+        <title>Team Profile</title>
     </head>
     <body>
     <div class="container-fluid">
     <div class="row">
-        <header class="jumbotron primary">My Team</header>
-        <p class='text-white'> Can't see me </p>`
+        <header class="jumbotron primary">My Team</header>`
 
         //for loop for adding to the html syntax
         for (let i = 0; i<employees.length; i++) {
